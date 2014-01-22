793a301509696d0661c4fbcb11e28ded9bcf47a1
Aaron <akrone@gmail.com>
2014-01-21 21:54:47 -0700

    modified:   ../DogeFit.ino 	new file:   MPU6050.cpp 	new file:   MPU6050.h 	new file:   MPU6050_6Axis_MotionApps20.h 	new file:   MPU6050_9Axis_MotionApps41.h 	new file:   helper_3dmath.h

--- DogeFit/DogeFit.ino
+++ DogeFit/DogeFit.ino
@@ -1,22 +1,11 @@
-
-
-#include <SeeedOLED.h>
-
-#include <I2Cdev.h>
-
 #include <GSM.h>
-
-
-#include <MPU6050.h>//add neccesary headfiles
+#include <MPU6050.h>// includes I2Cdev.h
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
 
 // DEFINES for Adafruit NEopixel
-
-#define N_PIXELS 6
+define N_PIXELS 6
 #define LED_PIN 15
-
-
 //====the offset of gyro===========
 #define Gx_offset  -1.50
 #define Gy_offset  0
@@ -31,7 +20,6 @@ MPU6050 accelgyro;
 Adafruit_NeoPixel  strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
  
-int test_int = 9;
 int16_t ax,ay,az;//original data;
 int16_t gx,gy,gz;//original data;
 float Ax,Ay,Az, Aplane, Azplane;//Unit g(9.8m/s^2)
@@ -41,38 +29,29 @@ float Dx, Dy, Dz;
 int Time; float Time_s;
 float DAUG, DAUGScore;
 
-//#define LED_PIN 13 
-//bool blinkState=false;
- 
 void setup()
 {
   strip.setBrightness(100);
   strip.begin();
   Wire.begin();
   Serial.begin(9600);
-   Serial1.begin(38400);
+  Serial1.begin(38400); // Serial1 is used for the Bluetooth
   
   Serial.println("Initializing I2C device.....");
   accelgyro.initialize();
   
   Serial.println("Testing device connections...");
   Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful":"MPU6050 connection failure");
-  //pinMode(LED_PIN,OUTPUT);
-  //accelgyro.setSleepEnabled(1);
-  
-  Wire.begin();
-  SeeedOled.init();  //initialze SEEED OLED display
+
+  // Needed for the Xadow BLE slave
   DDRB|=0x21;        
   PORTB |= 0x21;
-DAUGScore = 0;
-  SeeedOled.clearDisplay();          //clear the screen and set start position to top left corner
-  SeeedOled.setNormalDisplay();      //Set display to normal mode (i.e non-inverse mode)
-  SeeedOled.setPageMode();           //Set addressing mode to Page Mode
-  SeeedOled.setTextXY(0,0);          //Set the cursor to Xth Page, Yth Column  
-  //SeeedOled.putString("Hello World!"); //Print the String
   
  //setting up initial variables
- Time = 100; Time_s = Time/1000; //first in milliseconds, second in seconds 
+  Time = 100; 
+  Time_s = Time/1000; //first in milliseconds, second in seconds 
+  DAUGScore = 0;
+
 }
 
 void loop()
